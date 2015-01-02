08994da6160b31f18cd4de0b4b9a10e4679c5088
Kevin Lambert <kevinslambert@hotmail.com>
2015-01-02 16:55:59 -0700

    Initial Commit

---
+++ pre-course-git/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pre-course-git
+//
+//  Created by Kevin Lambert on 1/2/15.
+//  Copyright (c) 2015 KSL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
